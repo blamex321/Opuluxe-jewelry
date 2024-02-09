20c14e16b070301c126beb9bf539b09cd7c8db26
blamex321 <laxman2207@outlook.com>
2024-02-10 01:12:53 +0530

    added response reroutting for checkout

--- screens/CheckoutAndPayment.js
+++ screens/CheckoutAndPayment.js
@@ -1,23 +1,54 @@
 import React, { useContext, useState, useEffect } from "react";
-import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import { View, Text, TextInput, Button, StyleSheet,Alert } from "react-native";
 import LoginContext from "../contexts/loginContext";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+import CartContext from "../contexts/CartContext";
 
 const CheckoutDetails = ({ navigation }) => {
   const [name, setName] = useState("");
   const [address, setAddress] = useState("");
   const { user } = useContext(LoginContext);
+  const { cartItems } = useContext(CartContext);
   const [phoneNumber, setPhoneNumber] = useState(user.phoneNumber);
 
   const handleCheckout = () => {
-    userData = {
+    const userData = {
       name,
       address,
       phoneNumber,
     };
+  
     // Save the user data to local storage
     AsyncStorage.setItem("user", JSON.stringify(userData));
+  
+    axios.post("http://localhost:3000/", {
+      userPhoneNumber: userData.phoneNumber,
+      userName: userData.name,
+      userAddress: userData.address,
+      cartItems: cartItems
+    }).then((response) => {
+      console.log(response);
+  
+      // Check if the response status is 200
+      if (response.status === 200) {
+        // Show checkout successful message (you can use an alert, toast, or a custom component)
+        alert("Checkout successful!");
+  
+        // Redirect to the home page using React Navigation
+        navigation.navigate("Categories");
+      } else {
+        // Handle other response statuses as needed
+        console.error("Checkout failed. Status: ", response.status);
+        // You may want to show an error message to the user
+      }
+    }).catch((error) => {
+      // Handle the error from the axios request
+      console.error("Error during checkout:", error);
+      // You may want to show an error message to the user
+    });
   };
+  
 
   return (
     <View style={styles.container}>
